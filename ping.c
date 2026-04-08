4bb550c67bda4592f93c588948c5cd3914d65fc0
Nikki1089 <Nicole04@iastate.edu>
2026-04-08 13:33:18 -0500

    Update ping.c

--- ping.c
+++ ping.c
@@ -6,45 +6,40 @@
 
 #include "ping.h"
 #include "Timer.h"
-#include "lcd.h"
+#include <inc/tm4c123gh6pm.h>
+#include <driverlib/interrupt.h>
+#include <stdint.h>
 
 // Global shared variables
 // Use extern declarations in the header file
 
 volatile uint32_t g_start_time = 0;
 volatile uint32_t g_end_time = 0;
+volatile uint32_t g_pulse_tick = 0;
+volatile uint32_t g_overflow = 0;
 volatile enum
-{
-    LOW, HIGH, DONE
-} g_state = LOW; // State of ping echo pulse
+{ LOW, HIGH, DONE } g_state = LOW; // State of ping echo pulse
 
 void ping_init(void)
 {
+    SYSCTL_RCGCGPIO_R |= 0x02;
+    SYSCTL_RCGCTIMER_R |= 0x08;
 
-    //enable clock to GPIO port B
-    SYSCTL_RCGCGPIO_R |= 0b000010;
-    while ((SYSCTL_PRGPIO_R & 0b000010) == 0)
-    {
-    };
-    SYSCTL_RCGCTIMER_R |= 0b001000;
-    while ((SYSCTL_RCGCTIMER_R & 0b001000) == 0)
-    {
-    };
-    GPIO_PORTB_DEN_R |= 0b001000;
+    while ((SYSCTL_PRGPIO_R & 0x02) == 0) {}
+    while ((SYSCTL_RCGCTIMER_R & 0x08) == 0) {}
+   
 
-    lcd_printf("0\n");
+    GPIO_PORTB_DIR_R &= ~0x08; //pb3 input
+    GPIO_PORTB_DEN_R |= 0x08;
     GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & ~0xf000) | 0x7000;
-    lcd_printf("1\n");
-    TIMER3_CFG_R = (TIMER3_CFG_R & ~0x07) | 0x00;
-    lcd_printf("2\n");
-    TIMER3_TBMR_R &= ~0x13;
-    lcd_printf("3\n");
+    TIMER3_CFG_R = 0x4;
+    TIMER3_TBMR_R &= ~0x3; // clears bits
+    TIMER3_TBMR_R |= 0x7; //capture
+    TIMER3_TBMR_R &= ~0x10; //count down
     GPIO_PORTB_AFSEL_R |= 0x8;
-    lcd_printf("4\n");
     TIMER3_IMR_R |= 0x100;
-    lcd_printf("5\n");
     TIMER3_CTL_R |= 0xd00;
-    lcd_printf("complete\n");
+
 
     IntRegister(INT_TIMER3B, TIMER3B_Handler);
 
@@ -57,22 +52,23 @@ void ping_init(void)
 void ping_trigger(void)
 {
     g_state = LOW;
+    g_overflow =0;
+    g_pulse_ticks = 0;
     // Disable timer and disable timer interrupt
     TIMER3_CTL_R &= ~0x100;
     TIMER3_IMR_R &= ~0x400;
     // Disable alternate function (disconnect timer from port pin)
     GPIO_PORTB_AFSEL_R &= ~0x8;
     GPIO_PORTB_DIR_R |= 0x8;
-
+    GPIO_PORTB_DATA_R &= ~0x08;
+    timer_waitMicros(5);
     // YOUR CODE HERE FOR PING TRIGGER/START PULSE
     GPIO_PORTB_DATA_R &= ~0x08;
-    GPIO_PORTB_DATA_R |= 0x08;
-    GPIO_PORTB_DATA_R &= ~0x08;
+    GPIO_PORTB_DIR_R &= ~0x08;
 
     // Clear an interrupt that may have been erroneously triggered
     TIMER3_ICR_R |= 0x400;
     // Re-enable alternate function, timer interrupt, and timer
-    GPIO_PORTB_DIR_R &= ~0x8;
     GPIO_PORTB_AFSEL_R |= 0x8;
     TIMER3_IMR_R |= 0x400;
     TIMER3_CTL_R |= 0x100;
@@ -90,9 +86,9 @@ void TIMER3B_Handler(void)
     // Clearing the interrupt: set the ICR bit (so that same event doesn't trigger another interrupt)
     // The rest of the code in the ISR depends on actions needed when the event happens.
 
-    if (TIMER3_MIS_R & 0x400)
+    if (TIMER3_MIS_R & 0x0400)
     {
-        TIMER3_ICR_R |= 0x400;
+        TIMER3_ICR_R |= 0x0400;
 //        lcd_printf("aeiou");
         switch (g_state)
         {
@@ -110,7 +106,32 @@ void TIMER3B_Handler(void)
     }
 
 }
+uint32_t ping_getPulseTicks(void){
+while(g_state != DONE){}
+if(g_start_time >= g_end_time){
+g_pulse_ticks = g_start_time - g_end_time;
+}
+else {
+g_overflow = 1;
+}
+return g_pulse_ticks;
+}
 
+float ping_getDistance(void)
+{
+    uint32_t pulse_ticks;
+    float time;
+    float distance;
+    pulse_ticks = ping_getPulseTicks();
+    time = pulse_ticks / 16000000;
+    distance = (time * 34300) / 2;
+    return distance;
+}
+ 
+
+    return (float)(g_end_time - g_start_time) * 10000;
+
+}
 uint32_t get_g_start_time(void){
     return g_start_time;
 }
@@ -119,12 +140,4 @@ uint32_t get_g_end_time(void){
     return g_end_time;
 }
 
-float ping_getDistance(void)
-{
-
-    // YOUR CODE HERE
-
-    return (float)(g_end_time - g_start_time) * 10000;
-
-}
 
